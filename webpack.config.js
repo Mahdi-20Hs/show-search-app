8ba706a9454683d1c5261ad05ef80a1b3b644d46
Mahdi <mahdihs395@gmail.com>
2022-09-16 11:59:40 +0300

    set up the project

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: 'src/main.js',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, "dist"),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\(.png|svg|jpg|jpeg|gif)$/i,
+        use: 'asset/resource'
+      }
+    ]
+  }
+}
